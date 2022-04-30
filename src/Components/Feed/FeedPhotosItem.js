d59fc526a26558aa05cd480dfca4d6dff8dd1fbc
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-04-29 21:38:14 -0300

    Components

---
+++ src/Components/Feed/FeedPhotosItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from '../Helper/Image';
+import styles from './FeedPhotsItem.module.css';
+
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
+  return (
+    <li className={styles.photo} onClick={handleClick}>
+      <Image src={photo.src} alt={photo.title} />
+      <span className={styles.visual}>{photo.acessos}</span>
+    </li>
+  );
+};
+
+export default FeedPhotosItem;
